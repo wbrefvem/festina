45badfe76d2c2b989429d62af44f1073190f2b5a
Will Refvem <wbrefvem@ncsu.edu>
2014-06-24 17:34:31 -0400

    Periodic update

--- festina/urls.py
+++ festina/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'festina.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^resume/$', include('resume.urls'))
+    url(r'^resume/$', include('resume.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
